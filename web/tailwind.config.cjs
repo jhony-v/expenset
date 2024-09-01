f573ab738c28bc6686a06ac93ca135eba942b311
Jhony Vega <jhonyvega.dev@gmail.com>
2024-09-01 12:39:53 -0500

    feat: move frontend to web and new styles

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,7 +4,7 @@ const { nextui } = require("@nextui-org/react");
 module.exports = {
   content: [
     "./app/**/*.{tsx,ts}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
