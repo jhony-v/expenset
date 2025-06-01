3e7a3e63b765e446162a2f3c718f9a2bff62b040
Jhony Vega <jhonyvega@howdy.com>
2025-06-01 17:43:25 -0500

    deploy

--- supabase/functions/read-inbox-messages/index.ts
+++ supabase/functions/read-inbox-messages/index.ts
@@ -70,7 +70,7 @@ Deno.serve(async (req) => {
 
     const connection = await pool.connect();
     const categories = (
-      await connection.queryObject`SELECT id, name FROM categories`
+      await connection.queryObject`SELECT id, name FROM category`
     ).rows as Array<{ id: number; name: string }>;
 
     connection.release();
